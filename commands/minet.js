f8512488a6fc7dd0deb30873f8608fac082d87c8
BlurryMeal <sagarg2606@gmail.com>
2020-11-11 21:31:17 +0530

    class 6

--- commands/minet.js
+++ commands/minet.js
@@ -5,7 +5,7 @@ module.exports = {
 	execute(message, args) {
 		const exampleEmbed = new Discord.MessageEmbed().setTitle('MACH');
 		exampleEmbed.setColor('#6699EE');
-		exampleEmbed.setDescription("Mach will test your skills, knowledge and determination to the bone, by putting participants to the test in a head-to-head combat. Any student from classes 7 to 11 is eligible to participate in the events - Programming, Design, Quiz, Web Development, Gaming, Group Discussion, Audio-Visual Editing and Photography. MINET seeks to prune out the best in every category and Mach could be your doorway to join MINET.")
+		exampleEmbed.setDescription("Mach will test your skills, knowledge and determination to the bone, by putting participants to the test in a head-to-head combat. Any student from classes 6 to 11 is eligible to participate in the events - Programming, Design, Quiz, Web Development, Gaming, Group Discussion, Audio-Visual Editing and Photography. MINET seeks to prune out the best in every category and Mach could be your doorway to join MINET.")
 		exampleEmbed.setTimestamp()
 		message.author.send(exampleEmbed);
 		message.channel.send('Check your DM, you have been sent a message explaing what MACH is.');
