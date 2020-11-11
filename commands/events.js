f2032a2eb6dc2a5dbd4c32ee114f1b8de7e73c5e
BlurryMeal <sagarg2606@gmail.com>
2020-11-11 19:51:15 +0530

    gd timings update

--- commands/events.js
+++ commands/events.js
@@ -13,7 +13,7 @@ module.exports = {
 				{ name: 'Design', value: 'Submission: 25th November ( 9:41 pm )', inline: true },
 				{ name: 'Web Development', value: 'Submission: 25th November ( 9:41 pm )', inline: true },
 				{ name: 'Gaming', value: '20th and 21st ( Timings will be provided later )', inline: true },
-				{ name: 'Group Discussion', value: '23rd November ( 6:00 pm - 8:00 pm ) and 24th November ( 5:30 pm - 7:30 pm )', inline: true },
+				{ name: 'Group Discussion', value: '23rd November ( 6:00 pm - 8:00 pm ) and 24th November ( 6:30 pm - 8:30 pm )', inline: true },
 				{ name: 'Quiz', value: '23rd November ( 5:15 pm - 5:45 pm ) and 24th November ( 3:00 pm - 5:00 pm )', inline: true },
 				{ name: 'Photography', value: 'Submission:25th November ( 9:41 pm )', inline: true },
 				{ name: 'AV Editing', value: 'Submission: 25th November ( 9:41 pm ) ', inline: true },
