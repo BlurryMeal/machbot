fe8f3c2fdfaed765e95f174adbe1a7b35c4684b4
BlurryMeal <sagarg2606@gmail.com>
2020-11-16 21:24:03 +0530

    prog

--- commands/programming.js
+++ commands/programming.js
@@ -7,6 +7,7 @@ module.exports = {
 
 		exampleEmbed.setColor('#6699EE');
 		exampleEmbed.setDescription('Seemingly complex, programming is one of the most elementary skills to have. Fully based on logic applied to real life scenarios, you will be given different problems to work your way out of, by writing a program. Apart from the format of the question (which will be specified with input and output formats), the question will be a simple relation of math and logic. Work that out and put into the required syntax and there you have it!')
+		.addField('Info:', "https://docs.google.com/document/d/1SAyLGAaPkm9O6h5TFF22zBO89_eqPT5lyLES5ji3OVk/edit?usp=sharing")
 		exampleEmbed.setTimestamp()
 
 		message.author.send(exampleEmbed);
