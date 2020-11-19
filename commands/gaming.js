95514580fe1651a447d1e00c118ca79053bf305d
BlurryMeal <sagarg2606@gmail.com>
2020-11-19 19:39:22 +0530

    gaming deets

--- commands/gaming.js
+++ commands/gaming.js
@@ -7,6 +7,10 @@ module.exports = {
 
 		exampleEmbed.setColor('#6699EE');
 		exampleEmbed.setDescription("Gaming is the art of playing video game and it requires skills and determination. It isn't as easy at is seems, one needs to have a good hand to eye coordination, reflexes and daily practice to be good at most of the important games available in the market. Participants have to compete against each other in various types of games in order to be a **pro gamer**.")
+		.addField('Round 1', 'Fortnite Zone Wars', inline = true)
+		.addField('Round 2', 'Fortnite Custom Matchmaking Solo', inline = true)
+		.addField('Round 3', 'Valorant Deathmatch', inline = true)
+		.addField('Round 4', 'Valorant Mode - 5v5', inline = true)
 		exampleEmbed.setTimestamp()
 
 		message.author.send(exampleEmbed);
