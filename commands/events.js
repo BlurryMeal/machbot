5992ece8072f3a668c0e9dc85a2587c719bb59f7
BlurryMeal <sagarg2606@gmail.com>
2020-11-21 08:29:50 +0530

    poggers

--- commands/events.js
+++ commands/events.js
@@ -12,9 +12,9 @@ module.exports = {
 				{ name: 'Programming', value: '24th November (2:30 pm - 5:00 pm)', inline: true },
 				{ name: 'Design', value: 'Submission: 27th November (9:41 pm)', inline: true },
 				{ name: 'Web Development', value: 'Submission: 27th November  9:41 pm)', inline: true },
-				{ name: 'Gaming', value: '25th and 26th November (Timings will be provided later)', inline: true },
+				{ name: 'Gaming', value: '25th November ( 3 Rounds ) ( 2:30 pm - 8 pm ) \n 26th November ( Valorant 5v5 ) ( 3pm onwards )', inline: true },
 				{ name: 'Group Discussion', value: '24th November (6:00 pm - 8:00 pm) and 27th November (6:30 pm - 8:30 pm)', inline: true },
-				{ name: 'Quiz', value: '24th November (5:15 pm - 5:45 pm) and 25th November (3:00 pm - 5:00 pm)', inline: true },
+				{ name: 'Quiz', value: '24th November (5:15 pm - 5:45 pm) and 27th November (3:00 pm - 5:00 pm)', inline: true },
 				{ name: 'Photography', value: 'Submission:27th November (9:41 pm)', inline: true },
 				{ name: 'AV Editing', value: 'Submission: 27th November (9:41 pm) ', inline: true },
 			)
