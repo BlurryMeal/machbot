dd3dd89efe6a1d6ebb07d9cae39fe808908fe0d1
BlurryMeal <sagarg2606@gmail.com>
2020-11-26 19:10:51 +0530

    timings and dates update

--- commands/events.js
+++ commands/events.js
@@ -9,12 +9,12 @@ module.exports = {
 			.setURL('https://minet.co/mach/events')
 			.setDescription('Here are the Dates for all of the events.Timings for all the events will be provided later.')
 			.addFields(
-				{ name: 'Programming', value: '28th November ', inline: true },
+				{ name: 'Programming', value: '28th November (9:00am - 11:30am) ', inline: true },
 				{ name: 'Design', value: 'Submission: 1st December (9:41 pm)', inline: true },
 				{ name: 'Web Development', value: 'Submission: 1st December (9:41 pm) ', inline: true },
-				{ name: 'Gaming', value: '28th November (Valorant Deathmatch) \n 30th November (3 Rounds)', inline: true },
-				{ name: 'Group Discussion', value: '29th November', inline: true },
-				{ name: 'Quiz', value: '28th November and 30th November', inline: true },
+				{ name: 'Gaming', value: '28th, 29th and 30th November ( do **.gaming** for timings )', inline: true },
+				{ name: 'Group Discussion', value: '29th November (11:30am - 2pm) (6:30pm - 8:30pm)  ', inline: true },
+				{ name: 'Quiz', value: '28th November (4:00pm - 5:00pm) and 30th November (4:00pm - 5:30pm)', inline: true },
 				{ name: 'Photography', value: 'Submission:1st December (9:41 pm)', inline: true },
 				{ name: 'AV Editing', value: 'Submission: 1st December (9:41 pm) ', inline: true },
 			)
--- commands/gaming.js
+++ commands/gaming.js
@@ -7,10 +7,10 @@ module.exports = {
 
 		exampleEmbed.setColor('#6699EE');
 		exampleEmbed.setDescription("Gaming is the art of playing video game and it requires skills and determination. It isn't as easy at is seems, one needs to have a good hand to eye coordination, reflexes and daily practice to be good at most of the important games available in the market. Participants have to compete against each other in various types of games in order to be a **pro gamer**.")
-		.addField('Round 1', 'Fortnite Zone Wars', inline = true)
-		.addField('Round 2', 'Fortnite Custom Matchmaking Solo', inline = true)
-		.addField('Round 3', 'Valorant Deathmatch', inline = true)
-		.addField('Round 4', 'Valorant Mode - 5v5', inline = true)
+		.addField('Round 1', 'Fortnite Zone Wars (28th November [6:00pm - 9:00pm]', inline = true)
+		.addField('Round 2', 'Fortnite Custom Matchmaking Solo (29th November [9am - 11am]', inline = true)
+		.addField('Round 3', 'Valorant Deathmatch (28th November [6:00pm - 9:00pm]', inline = true)
+		.addField('Round 4', 'Valorant Mode - 5v5 (30th November [3:00pm - 9:00pm])', inline = true)
 		exampleEmbed.setTimestamp()
 
 		message.author.send(exampleEmbed);
